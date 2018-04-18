bb0dd3740c6c8d5a1ae50ec6c041ccd04ca0fcbf
bruno_quaresma <bruno_nonato_quaresma@hotmail.com>
2018-04-18 20:52:31 -0300

    Fix human readble

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,24 @@ module ApplicationHelper
     print_time(time_in_seconds / 60.to_f)
   end
 
+  def print_human_time(seconds)
+    if seconds < 60
+      return "#{seconds} segundos" if seconds != 1
+      return "#{seconds} segundo"
+    end
+
+    minutes = seconds / 60
+    rest_seconds = seconds % 60
+
+    return "#{minutes} minutos e #{rest_seconds} segundos" if minutes != 1
+
+    "#{minutes} minuto e #{rest_seconds} segundos"
+  end
+
+  def print_short_time(seconds)
+    Time.at(seconds).utc.strftime("%H:%M:%S")
+  end
+
   def letter_option(index)
     return nil if index.nil?
 
