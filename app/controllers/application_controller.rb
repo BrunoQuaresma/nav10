2402a3b122bb732e014b47eadc454de7450ef8ba
bruno_quaresma <bruno_nonato_quaresma@hotmail.com>
2018-04-22 19:00:53 -0300

    Redirect student after login to exams

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+
+    def after_sign_in_path_for(user)
+      user.student? ? panel_student_exams_path : panel_exam_applications_path
+    end
 end
